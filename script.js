36a060f4854734337352832ce6731f3cc97ecc81
SimplyWhitehouse <SimplyWhitehouse@gmail.com>
2020-03-26 10:45:30 -0500

    Made it work
    index.html
    - Add win message, reference sha256 script, and put styling in head
    
    script.js
    - Add answer logging, validation, and show if wrong or win
    - Use sha256 encryption so they can't just look at the code for the
    answer
    
    sha256.js
    - Add this file to have a sha256 encrypter

--- script.js
+++ script.js
@@ -1,11 +1,52 @@
 var pinpad = document.querySelector(".pinpad");
 var buttons = pinpad.querySelectorAll("button");
 var enterButton = document.getElementById("enterButton");
+var display = document.getElementById("display");
+var winMessage = document.getElementById("winMessage");
+
+function makeButtonHandler(num){
+    return function(){handleNumberPress(num)}
+}
 
 function handleNumberPress(num){
-    console.log(num);
+    currentAnswer += "" + num;
+    if (currentAnswer.length > 5) {
+        currentAnswer = currentAnswer.slice(1);
+    }
+    updateDisplay();
+}
+
+function updateDisplay(){
+    display.value = currentAnswer;
+}
+
+var currentAnswer = "";
+var encryptedAnswer = "5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5";
+
+function answerIsCorrect(){
+    return sha256(currentAnswer) === encryptedAnswer;
+}
+
+function showWrongAnswer(){
+    pinpad.classList.add("wrongAnswer");
+    setTimeout(function(){pinpad.classList.remove("wrongAnswer");}, 500);
+    currentAnswer = "";
+    updateDisplay();
+}
+
+function showWin(){
+    winMessage.classList.remove("hidden");
 }
 
 for (var i = 0; i < 10; i++){
-    buttons[i].onclick = function(){handleNumberPress((i+1)%10)}
+    var j = (i+1)%10;
+    buttons[i].onclick = makeButtonHandler(j);
+}
+
+enterButton.onclick = function(){
+    if (answerIsCorrect()) {
+        showWin();
+    } else {
+        showWrongAnswer();
+    }
 }
