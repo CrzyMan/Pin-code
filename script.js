2e20f54bd5cbaf33d8157c7cd2c7049d77620186
SimplyWhitehouse <SimplyWhitehouse@gmail.com>
2020-04-03 16:07:48 -0400

    Add ability to use url instead of default pincode and add documentation

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
+/***** Start DOM variables *****/
 var pinpad = document.querySelector(".pinpad");
 var buttons = pinpad.querySelectorAll("button");
 var enterButton = document.getElementById("enterButton");
 var display = document.getElementById("display");
 var winMessage = document.getElementById("winMessage");
+/***** End DOM variables *****/
 
+
+
+/***** Start handling DOM interactions *****/
 function makeButtonHandler(num){
     return function(){handleNumberPress(num)}
-}
-
-function handleNumberPress(num){
-    currentAnswer += "" + num;
-    if (currentAnswer.length > 5) {
-        currentAnswer = currentAnswer.slice(1);
-    }
-    updateDisplay();
 }
 
 function updateDisplay(){
     display.value = currentAnswer;
 }
+/***** End handling DOM interactions *****/
 
+
+
+/***** Start business logic *****/
 var currentAnswer = "";
 var encryptedAnswer = "8ef6e5ea68e21ce2f45ff7fb7614b57421b5e7d6b21d793d70178945af061c3b";
+(function(){
+	// If key is included
+	if (window.location.search.indexOf("?key=")){
+		const key = window.location.search.slice(5);
+		if (key.length == encryptedAnswer.length){
+			encryptedAnswer = key;
+		}
+	}
+})()
 
 function answerIsCorrect(){
     return sha256(currentAnswer) === encryptedAnswer;
@@ -34,14 +44,25 @@ function showWrongAnswer(){
     updateDisplay();
 }
 
+const maxAnswerLength = 5;
+function handleNumberPress(num){
+    currentAnswer += "" + num;
+    if (currentAnswer.length > maxAnswerLength) {
+        currentAnswer = currentAnswer.slice(1);
+    }
+    updateDisplay();
+}
+
 function showWin(){
     winMessage.classList.remove("hidden");
 }
 
-for (var i = 0; i < 10; i++){
-    var j = (i+1)%10;
-    buttons[i].onclick = makeButtonHandler(j);
-}
+(()=>{
+	for (var i = 0; i < 10; i++){
+		var j = (i+1)%10;
+		buttons[i].onclick = makeButtonHandler(j);
+	}
+})()
 
 enterButton.onclick = function(){
     if (answerIsCorrect()) {
@@ -50,3 +71,4 @@ enterButton.onclick = function(){
         showWrongAnswer();
     }
 }
+/***** End business logic *****/
